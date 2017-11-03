eb9d3269bfdb0194a6b3eaf2b3a657c3d4c09a94
Seth DeVries <sethdv@cs.uchicago.edu>
2017-11-02 23:12:49 -0500

    Problem 001

--- Haskell/Problem001.hs
+++ Haskell/Problem001.hs
@@ -2,13 +2,13 @@ module Main where
 
 main :: IO ()
 main = do
-    putStrLn $ show $ multiplesSum
+    putStrLn $ show $ sumMults
 
 divByEither :: Int -> Int -> Int -> Bool
 divByEither a b n = (n `mod` a) == 0 || (n `mod` b) == 0
 
-multiplesSum :: Int
-multiplesSum = let
+sumMults :: Int
+sumMults = let
                    values = [1 .. 999]
                    culled = filter (divByEither 5 3) $ values
                    total  = sum culled
