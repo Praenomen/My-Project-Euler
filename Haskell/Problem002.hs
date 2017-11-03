f8c74ddf959932379828fc38a365a20b0be6bb5f
Seth DeVries <sethdv@cs.uchicago.edu>
2017-11-02 23:11:28 -0500

    Problem 002

--- Haskell/Problem002.hs
+++ Haskell/Problem002.hs
@@ -4,14 +4,14 @@ main :: IO ()
 main = do
     putStrLn $ show $ sumVals
 
-fibs :: [Int] -> [Int]
-fibs a = if (head a) <= 4000000
-         then fibs $ ((head a) + (head $ tail a)):a
+fibber :: [Int] -> [Int]
+fibber a = if (head a) <= 4000000
+         then fibber $ ((head a) + (head $ tail a)):a
          else a
 
 sumVals :: Int
 sumVals = let
-              fibSeq = fibs [1,1]
+              fibSeq = fibber [1,1]
               culled = filter even $ filter (<= 4000000) fibSeq
               total  = sum culled
           in total
